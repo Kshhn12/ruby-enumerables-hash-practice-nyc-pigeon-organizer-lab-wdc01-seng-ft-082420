95fdf8c83eaac236c090b63bf4b93836c35b1371
Kevin Sheehan <kshhn12@gmail.com>
2020-08-15 17:04:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,8 +5,13 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), new_array|
     value.each do |new_key, names|
       names.each do |name|
-        binding.pry
-        if(!)
+        #binding.pry
+        if(!new_array[name])
+          new_array[name] = {}
+        end
+      end
+      if !new_array[name][key]
+        !new_array[name][key] = []
       end
     end
     new_array
